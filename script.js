5b3fcfc314ea9823d871539f0ef5aa2b06104f32
Ryan Carpenter <scrian_12@yahoo.com>
2020-12-07 15:31:47 -0500

    changed to match required directions, displays single breed

--- script.js
+++ script.js
@@ -1,30 +1,22 @@
 function generateDogs(){
-    let url = 'https://dog.ceo/api/breeds/image/random/' + $('.numberInput').val();
+    //let breed = $('.breed-input').val();
+    let url = 'https://dog.ceo/api/breed/' + $('.breed-input').val() + '/images/random';
     console.log(url);
     fetch(url)
     .then(response => response.json())
     .then(responseJson => displayImages(responseJson))
-    .catch(error => console.log(error)); //alert('uh-oh, that was NOT supposed to happen'));
+    .catch(alert('breed not found')); //alert('uh-oh, that was NOT supposed to happen'));
 }
 
 
 function displayImages(responseJson) {
     console.log(responseJson);
     //let num = $('.numberInput').val();
-if($('.numberInput').val() == 1){
+if($('.breed-input').val()){
     $('.results-img').replaceWith(
         `<img src='${responseJson.message}' class = 'results-img'>`
     )
-} else{
-    let imageList = '';
-    //let response = jquery.parseJSON(responseJson);
-    let message = responseJson.message;
-    for(let i = 0;  i < message.length; i++){
-        $('.results-img').append(`<img src='${message[i]}' class = 'results-img'>`);
-    }
-}
-
-
+} 
 
 
     $('.images').removeClass('hidden');
